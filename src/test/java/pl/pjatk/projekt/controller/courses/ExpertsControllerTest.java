8537619f0d312bcfa4ecec6ed4444c750753d553
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-18 14:42:20 +0100

    class renamed to reflect exactly name of ExpertsController

--- src/test/java/pl/pjatk/projekt/controller/courses/ExpertsControllerTest.java
+++ src/test/java/pl/pjatk/projekt/controller/courses/ExpertsControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static pl.pjatk.projekt.controller.AbstractRestControllerTest.asJsonString;
 
-public class ExpertControllerTest {
+public class ExpertsControllerTest {
 
     @Mock
     ExpertServiceImpl expertService;
