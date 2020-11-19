60cd194216b2cd3a27897650d28182e4f072d5c7
adrianromanski <adrianromanski@icloud.com>
2020-11-19 19:57:08 +0100

    #41 Fixed Small Issues with Mapstruct

--- src/main/java/pl/pjatk/projekt/mapstruct/courses/LecturesMapper.java
+++ src/main/java/pl/pjatk/projekt/mapstruct/courses/LecturesMapper.java
@@ -2,11 +2,14 @@ package pl.pjatk.projekt.mapstruct.courses;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 import pl.pjatk.projekt.dto.courses.LectureDTO;
 import pl.pjatk.projekt.model.courses.Lecture;
 
 @Mapper
 public interface LecturesMapper {
+
+    LecturesMapper INSTANCE = Mappers.getMapper(LecturesMapper.class);
 
     @Mapping(target = "expert", source = "expertDTO")
     Lecture lectureDTOToLecture(LectureDTO lectureDTO);
