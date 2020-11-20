c159b7ce27d8949d94fb8180a9a71061b876053f
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-20 16:58:56 +0100

    LecturesMapperTest with nested entitites

--- src/test/java/pl/pjatk/projekt/mapstruct/courses/LecturesMapperTest.java
+++ src/test/java/pl/pjatk/projekt/mapstruct/courses/LecturesMapperTest.java
@@ -2,8 +2,10 @@ package pl.pjatk.projekt.mapstruct.courses;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pl.pjatk.projekt.dto.courses.ExpertDTO;
 import pl.pjatk.projekt.dto.courses.LectureDTO;
 import pl.pjatk.projekt.enums.Category;
+import pl.pjatk.projekt.model.courses.Expert;
 import pl.pjatk.projekt.model.courses.Lecture;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -40,5 +42,43 @@ public class LecturesMapperTest {
 
         // then
         assertEquals(lectureDTO.getCategory(), returnObj.getCategory());
+    }
+
+    @Test
+    @DisplayName("")
+    void lectureToLectureDTONestedEntities() {
+        // given
+        Expert expert = Expert.builder()
+                .firstName("Janusz")
+                .build();
+
+        Lecture lecture = Lecture.builder()
+                .expert(expert)
+                .build();
+
+        // when
+        LectureDTO returnObj = mapper.lectureToLectureDto(lecture);
+
+        // then
+        assertEquals(lecture.getExpert().getFirstName(), returnObj.getExpertDTO().getFirstName());
+    }
+
+    @Test
+    @DisplayName("")
+    void lectureDTOToLectureNestedEntities() {
+        // given
+        ExpertDTO expertDTO = ExpertDTO.builder()
+                .firstName("Aleksandra")
+                .build();
+
+        LectureDTO lectureDTO = LectureDTO.builder()
+                .expertDTO(expertDTO)
+                .build();
+
+        // when
+        Lecture returnObj = mapper.lectureDTOToLecture(lectureDTO);
+
+        // then
+        assertEquals(lectureDTO.getExpertDTO().getFirstName(), returnObj.getExpert().getFirstName());
     }
 }
