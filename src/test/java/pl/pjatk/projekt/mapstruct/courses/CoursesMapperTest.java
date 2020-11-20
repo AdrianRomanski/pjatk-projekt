74a245a47bd3ca8173655b3f2056f8de722b08df
Paweł Dąbrowski <fluorix@gmail.com>
2020-11-20 17:17:55 +0100

    CoursesMapperTest checked for nested entities

--- src/test/java/pl/pjatk/projekt/mapstruct/courses/CoursesMapperTest.java
+++ src/test/java/pl/pjatk/projekt/mapstruct/courses/CoursesMapperTest.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pl.pjatk.projekt.dto.courses.CourseDTO;
+import pl.pjatk.projekt.dto.courses.LectureDTO;
+import pl.pjatk.projekt.enums.Category;
 import pl.pjatk.projekt.model.courses.Course;
+import pl.pjatk.projekt.model.courses.Lecture;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CoursesMapperTest {
 
@@ -22,7 +29,7 @@ class CoursesMapperTest {
         CourseDTO returnDTO = mapper.courseToCourseDTO(course);
 
         //then
-        Assertions.assertEquals(course.getName(), returnDTO.getName());
+        assertEquals(course.getName(), returnDTO.getName());
     }
 
     @Test
@@ -37,6 +44,44 @@ class CoursesMapperTest {
         Course returnObj = mapper.courseDTOtoCourse(courseDTO);
 
         //then
-        Assertions.assertEquals(courseDTO.getName(), returnObj.getName());
+        assertEquals(courseDTO.getName(), returnObj.getName());
+    }
+
+    @Test
+    @DisplayName("")
+    void courseDTOToCourseNestedEntities() {
+        // given
+        LectureDTO lectureDTO = LectureDTO.builder()
+                .category(Category.ELECTRONICS)
+                .build();
+
+        CourseDTO courseDTO = CourseDTO.builder()
+                .lecturesDTO(Collections.singletonList(lectureDTO))
+                .build();
+
+        // when
+        Course returnObj = mapper.courseDTOtoCourse(courseDTO);
+
+        // then
+        assertEquals(lectureDTO.getCategory(), returnObj.getLectures().get(0).getCategory());
+    }
+
+    @Test
+    @DisplayName("")
+    void courseToCourseDTONestedEntites() {
+        // given
+        Lecture lecture = Lecture.builder()
+                .availableDuringCovid(true)
+                .build();
+
+        Course course = Course.builder()
+                .lectures(Collections.singletonList(lecture))
+                .build();
+
+        // when
+        CourseDTO returnDTO = mapper.courseToCourseDTO(course);
+
+        // then
+        assertEquals(lecture.getAvailableDuringCovid(), returnDTO.getLecturesDTO().get(0).getAvailableDuringCovid());
     }
 }
